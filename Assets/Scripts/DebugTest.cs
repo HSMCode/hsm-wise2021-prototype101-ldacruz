41cbd31072913cd25258a62ce14902e1b91c2334
Linda Kruse (HSM) <linda.kruse@hs-mainz.de>
2021-11-05 15:06:46 +0100

    add debug script

---
+++ Assets/Scripts/DebugTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello. I'm a debug log. Juhu!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Debug.Log("Hello. Every frame, I say - Hello!");
+    }
+}
