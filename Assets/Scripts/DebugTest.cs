c0dccda1f1aec27f487ece03c7362ddf64453bb4
Linda Kruse (HSM) <linda.kruse@hs-mainz.de>
2021-11-12 17:32:42 +0100

    update debug script with error and warning methods

--- Assets/Scripts/DebugTest.cs
+++ Assets/Scripts/DebugTest.cs
@@ -7,12 +7,16 @@ public class DebugTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // various debug log methods
         Debug.Log("Hello. I'm a debug log. Juhu!");
+        Debug.LogError("ERROR - Hello. I'm a debug log. Juhu!");
+        Debug.LogWarning("WARNING. I'm a debug log. Juhu!");
     }
 
     // Update is called once per frame
     void Update()
     {
-       Debug.Log("Hello. Every frame, I say - Hello!");
+        // This is a debug message, that will be displayed every frame, when uncommented.
+        //  Debug.Log("Hello. Every frame, I say - Hello!"); 
     }
 }
