e3c2d86af237b72e2a327fbc2b25729108dcd02e
ldacruz <bitbucket@lindakruse.de>
2021-11-28 18:16:35 +0100

    add spawner script to instantiate objects on button press

---
+++ Assets/Scripts/Prototype101/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnObject;
+
+    void Update()
+    {
+        // to test the spawn method, use S to spawn an object
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            spawningObject();
+        }
+    }
+    private void spawningObject()
+    {
+        // clone prefab of game object
+        Instantiate(spawnObject, transform.position, spawnObject.transform.rotation);
+        Debug.Log("Object spawned!");   
+    }
+}
