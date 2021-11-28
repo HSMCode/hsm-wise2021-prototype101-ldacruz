7ec71ef05ccf0d5fe74ad1c97b7ea086b72509ff
ldacruz <bitbucket@lindakruse.de>
2021-11-28 18:16:59 +0100

    add spawner script to invoke objects automatically each x seconds

---
+++ Assets/Scripts/Prototype101/SpawnerAutomatic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerAutomatic : MonoBehaviour
+{
+    public GameObject invokedObject;
+
+    void Start()
+    {
+        // this method calls the custom method "InvokeObject" starting after 5 seconds, and then every 2 seconds 
+        InvokeRepeating("InvokeObject", 5.0f, 2.0f);
+    }
+    
+    private void InvokeObject()
+    {
+        // clone prefab of game object
+        Instantiate(invokedObject, transform.position, invokedObject.transform.rotation);
+        Debug.Log("Object automatically invoked on repeat!");   
+    }
+}
