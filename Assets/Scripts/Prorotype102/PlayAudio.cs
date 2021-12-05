000002865e637ce460f8f2da289e2f768a72f029
ldacruz <bitbucket@lindakruse.de>
2021-12-05 03:38:31 +0100

    add scripts to play audio and particles with custom methods

---
+++ Assets/Scripts/Prorotype102/PlayAudio.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+public class PlayAudio : MonoBehaviour
+{
+    // first is the variable to save the audio source reference
+    // second is the variable for an array to assign the sfx sound clips
+    private AudioSource _playerAudioSource;
+    public AudioClip[] clips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // We need to get the audio source component assigned on the player and save it in the variable.
+        _playerAudioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudioClip()
+    {
+        //the audio clip is only played, as long as there is no audio currently playing
+        if (!_playerAudioSource.isPlaying)
+        {
+            // We create another int variable just inside this method, to randomly select one clip from the audio array
+            int randomClip = Random.Range(0, clips.Length);
+
+            // This sets the randomly picked array number on the audio source as current clip (to play)
+            _playerAudioSource.clip = clips[randomClip];
+
+            // The audio source 
+            _playerAudioSource.Play();
+        }
+    }
+
+    public void ShotAudio(AudioClip clip)
+    {
+        _playerAudioSource.PlayOneShot(clip, 1.0f);
+    }
+}
---
+++ Assets/Scripts/Prorotype102/UseParticles.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class UseParticles : MonoBehaviour
+    {
+        public ParticleSystem playParticleSystem;
+        public ParticleSystem emitParticleSystem;
+
+        public void PlayingParticles(bool on)
+        {
+            if (on)
+            {
+                playParticleSystem.Play();                 
+            } 
+            else if(!on)
+            {
+                playParticleSystem.Stop();
+            }
+        }
+        
+        public void EmitParticles()
+        {
+            emitParticleSystem.Emit(5);
+        }
+    }
+}
