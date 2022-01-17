1a264b7d5e5b842b91fbf860022b6802d26b93af
ldacruz <bitbucket@lindakruse.de>
2022-01-17 04:20:07 +0100

    add game manager to use static variables

---
+++ Assets/Scripts/Prototype105/GameManagerStatic.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManagerStatic
+{
+    public static int ScoreStatic { get; set; }
+    public static bool GameOverStatic { get; set; }
+}
