d631814731aa6930239ee4e797caa228f9256225
ldacruz <bitbucket@lindakruse.de>
2022-01-17 04:20:30 +0100

    add game manager to make use of scriptable objects

---
+++ Assets/Scripts/Prototype105/GameManagerScriptableObjects/FloatVariable.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Float", menuName = "ScriptableObjects/FloatVariable", order = 51)]
+public class FloatVariable : ScriptableObject
+{
+    public float value;
+}
+
+
---
+++ Assets/Scripts/Prototype105/GameManagerScriptableObjects/IntegerVariable.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Integer", menuName = "ScriptableObjects/IntegerVariable", order = 51)]
+public class IntegerVariable : ScriptableObject
+{
+    public int value;
+}
+
+
