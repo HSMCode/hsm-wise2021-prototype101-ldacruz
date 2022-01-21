10c55e04bbed455131d32eed9bf33e4211ab12f2
ldacruz <bitbucket@lindakruse.de>
2022-01-21 16:02:29 +0100

    add scriptable objects for letter and word objects

---
+++ Assets/Scripts/Prototype106/ScriptableObjectsLetters/LetterStates.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Letter", menuName = "ScriptableObjects/LetterStates", order = 101)]
+public class LetterStates : ScriptableObject
+{
+    public List<Letters> letterStates;
+} 
+
+
---
+++ Assets/Scripts/Prototype106/ScriptableObjectsLetters/Letters.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Letter", menuName = "ScriptableObjects/Letters", order = 101)]
+public class Letters : ScriptableObject
+{
+    public GameObject blueLetter;
+    public GameObject yellowLetter;
+}
+
+
